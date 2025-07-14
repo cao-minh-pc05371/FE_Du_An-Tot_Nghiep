a4f0ca3521bd789749a15812f5317751ce7176ad
cao-minh-pc05371 <minhncpc05371@fpt.edu.vn>
2025-07-14 17:30:34 +0700

    Fix lỗi TiviSection

--- src/pages/Client/Home/Tivi/Tivi.jsx
+++ src/pages/Client/Home/Tivi/Tivi.jsx
@@ -62,7 +62,6 @@ const Tivi = () => {
               key={index}
               className="whitespace-nowrap px-4 py-2 bg-gray-100 rounded-full text-sm text-gray-700 hover:bg-red-100 transition"
             >
-              className="px-3 sm:px-4 py-1.5 sm:py-2 bg-gray-100 rounded-full text-xs sm:text-sm text-gray-700 hover:bg-red-100 transition whitespace-nowrap">
               {brand}
             </button>
           ))}
